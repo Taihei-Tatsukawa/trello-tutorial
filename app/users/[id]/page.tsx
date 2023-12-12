6249120850242f4cd69fa9e19106ede3054e87e7
Tatsukawa <takun.cloud961@gmail.com>
2023-12-13 01:33:22 +0900

    WIP ルーティング確認

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,18 @@
+const IdPage = ({
+  params,
+}: {
+  params: {
+    id: string
+  }
+}
+
+) => {
+
+  return (
+    <div>
+      <h1>Id: {params.id}</h1>
+    </div>
+  )
+}
+
+export default IdPage
